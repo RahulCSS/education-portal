2143737665bf08f99c691f39891a7a46c7ae27e4
Rahul <123063375+RahulCSS@users.noreply.github.com>
2025-04-26 19:36:58 +0530

    Added homepage header structure

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header-container">
+        <div className="header">
+            <img src="./src/assets/homepage_poster.jpeg" alt="Image"/>
+            <div className="header_float">
+            <p>Learning that gets you</p>
+            <p>Skills for your present (and your future). Get started.</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
