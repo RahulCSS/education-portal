3cf0f39a2cf18ab0d43bd2c674ddad637d102491
Rahul <123063375+RahulCSS@users.noreply.github.com>
2025-04-26 13:42:31 +0530

    Added Google fonts and navbar structuring

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Routes, Route } from "react-router";
 import Home from './pages/home/home'
 import Navbar from './components/navbar/Navbar';
+import './App.css'
 
 const App = () => {
   return (
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { PersonIcon, SunIcon, ReaderIcon } from "@radix-ui/react-icons"
+import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+        <div className="navbar">
+                <div className='title'>
+                    <img src="./src/assets/brain.png" alt="logo"/>
+                    <a>Elevate</a>
+                    <a>lms</a>
+                </div>
+                <ul className="menu">
+                    <li>Explore</li>
+                    <li>Academy</li>
+                    <li>AboutUs</li>
+                    <li>ContactUs</li>
+                </ul>
+            <div className='profile'>
+                <a><SunIcon /></a>
+                <a><PersonIcon /></a>
+                <a><ReaderIcon /></a>
+            </div>
+        </div>
   )
 }
 
