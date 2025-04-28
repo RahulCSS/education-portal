69620bae732fc98f078e89e16ecc3e35f4ffa61b
Rahul <123063375+RahulCSS@users.noreply.github.com>
2025-04-28 13:47:19 +0530

    Added Redux store

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -37,25 +37,25 @@ const compareHashValue = async (value, hashedValue) => {
 
 // Register a new user
 const signupUser = async (req,res) => {
-    const {full_name, email, password } = req.body;
+    const {fullname, email, password } = req.body;
     try{
         // 1. Check if all required fields are provided
-        if(!first_name || !last_name || !email || !password){
-            return res.status(400).json({ message: "Please fill all the fields" });
+        if(!fullname || !email || !password){
+            return res.status(400).json({ success: false, message: "Please fill all the fields" });
         }
 
         // 2. Validating fields
         if(!isValidEmail(email)){
-            return res.status(400).json({ message: "Please enter a valid email" });
+            return res.status(422).json({ success: false, message: "Please enter a valid email" });
         }
         if(!isValidPassword(password)){
-            return res.status(400).json({ message: "Password must be at least 8 characters long, must contain at least one uppercase letter, one lowercase letter, and one number" });
+            return res.status(422).json({ success: false, message: "Password must be at least 8 characters long, must contain at least one uppercase letter, one lowercase letter, and one number" });
         }
 
         // 3. If all fields valid , Check if email already exists
         const userExists = await userModel.findOne({ email });
         if(userExists){
-            return res.status(400).json({ message: "User already exists" });
+            return res.status(409).json({ success: false, message: "User already exists" });
         }
 
         // 4. Hash the password
@@ -63,25 +63,24 @@ const signupUser = async (req,res) => {
 
         // 5. Create a new user
         const newUser = new userModel({
-            full_name,
+            fullname,
             email,
             password: hashedPassword,
             role: "Student"
         });
         await newUser.save();
-        return res.status(201).json({ message: "User registered successfully" });
+        return res.status(201).json({ success: true, message: "User registered successfully" });
     }catch (error) {
-        console.error("Error registering user:", error);
-        return res.status(500).json({ message: "Internal server error" });
+        return res.status(500).json({ message: "Error registering user, Internal sever error.", error});
     }
 };
 
 // Register a new user
 const registerUser = async (req,res) => {
-    const {full_name, email, password, role} = req.body;
+    const {fullname, email, password, role} = req.body;
     try{
         // 1. Check if all required fields are provided
-        if(!first_name || !last_name || !email || !password){
+        if(!fullname || !email || !password){
             return res.status(400).json({ message: "Please fill all the fields" });
         }
 
@@ -104,7 +103,7 @@ const registerUser = async (req,res) => {
 
         // 6. Create a new user
         const newUser = new userModel({
-            full_name,
+            fullname,
             email,
             password: hashedPassword,
             role,
