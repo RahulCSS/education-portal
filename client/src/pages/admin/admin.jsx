3cbd8cc32c650977b246610301844d0581bcb625
Rahul <123063375+RahulCSS@users.noreply.github.com>
2025-05-02 19:59:14 +0530

    Added Admin page

--- client/src/pages/admin/admin.jsx
+++ client/src/pages/admin/admin.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './admin.css'
+import Dashboard from '../../components/adminpage/dashboard/Dashboard';
 
 const Admin = () => {
+  const menu = [
+    'Dashboard',
+    'Students',
+    'Tutors',
+    'Courses',
+    'Announcements',
+    'Queires',
+  ]
   return (
-    <div>admin</div>
+    <div className="admin-container">
+      <div className="admin-grid">
+        <div className="sidebar">
+          <p>LMS Admin</p>
+          {menu.map((items,index)=> {
+            return <span key={index}>{items}</span>
+          })}
+        </div>
+          <div className="topbar">
+            Menu
+          </div>
+          <div className="content">
+            <Dashboard />
+          </div>
+      </div>
+    </div>
   )
 }
 
