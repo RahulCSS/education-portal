7c60e5036318e747671e1fbf0a62d7343cb3cbdd
Rahul <123063375+RahulCSS@users.noreply.github.com>
2025-05-02 21:55:51 +0530

    Added icons

--- client/src/pages/admin/admin.jsx
+++ client/src/pages/admin/admin.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
 import './admin.css'
 import Dashboard from '../../components/adminpage/dashboard/Dashboard';
+import { DashboardIcon, AvatarIcon, PersonIcon, BellIcon, ArchiveIcon, QuestionMarkCircledIcon,  } from "@radix-ui/react-icons"
 
 const Admin = () => {
   const menu = [
-    'Dashboard',
-    'Students',
-    'Tutors',
-    'Courses',
-    'Announcements',
-    'Queires',
-  ]
+    ['Dashboard',<DashboardIcon />],
+    ['Students',<AvatarIcon />],
+    ['Tutors',<PersonIcon />],
+    ['Courses',<ArchiveIcon />],
+    ['Announcements',<BellIcon />],
+    ['Queires',<QuestionMarkCircledIcon />]
+  ];
+
   return (
     <div className="admin-container">
       <div className="admin-grid">
         <div className="sidebar">
           <p>LMS Admin</p>
           {menu.map((items,index)=> {
-            return <span key={index}>{items}</span>
+            return (
+              <div key= {index} className='menu-icon'>
+                <a>{items[1]}</a>
+                <span>{items[0]}</span>
+              </div>
+            )
           })}
         </div>
           <div className="topbar">
