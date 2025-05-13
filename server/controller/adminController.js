66277935e7bd0baced6abee4b399877ef31a2a49
Rahul <123063375+RahulCSS@users.noreply.github.com>
2025-05-13 09:54:01 +0530

    Tutor API

--- server/controller/adminController.js
+++ server/controller/adminController.js
@@ -29,9 +29,27 @@ const getStudents = async (req,res) => {
 }
 
 const getTutors = async (req,res) => {
+    const {
+        page = 1,
+        limit = 5,
+        sortBy = 'fullname',
+        sortOrder = 'asc',
+        search = ''
+      } = req.query;
+    const pageNum = parseInt(page);
+    const limitNum = parseInt(limit);
+    
+    const total = await userModel.countDocuments({role: 'Student'});
+    const totalPages = Math.ceil(total / limitNum);
+
     try{
-        const tutors = await userModel.find({role: 'Tutor'}).select("fullname email phone createdAt")
-        return res.status(200).json({ success: true, data: tutors });
+        const students = await userModel
+            .find({role: 'Tutor'})
+            .select("fullname email phone createdAt address.state")
+            .sort({ [sortBy]: sortOrder === 'asc' ? 1 : -1 })
+            .skip((pageNum - 1) * limitNum)
+            .limit(limitNum);
+        return res.status(200).json({ success: true, data: students, totalpages:totalPages });
     }catch (error) {
         console.error(error);
         return res.status(500).json({ success: false, message: "Error fetching students" });
